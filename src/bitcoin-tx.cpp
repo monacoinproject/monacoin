cab5a455b0bd2a04d01419b00bc3887321f03eea
Daniel Cousens <github@dcousens.com>
2015-10-18 21:02:36 +1100

    *: alias -h for --help

--- src/bitcoin-tx.cpp
+++ src/bitcoin-tx.cpp
@@ -44,7 +44,7 @@ static bool AppInitRawTx(int argc, char* argv[])
 
     fCreateBlank = GetBoolArg("-create", false);
 
-    if (argc<2 || mapArgs.count("-?") || mapArgs.count("-help"))
+    if (argc<2 || mapArgs.count("-?") || mapArgs.count("-h") || mapArgs.count("-help"))
     {
         // First part of help message is specific to this utility
         std::string strUsage = _("Monacoin Core monacoin-tx utility version") + " " + FormatFullVersion() + "\n\n" +
