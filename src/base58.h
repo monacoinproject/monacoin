aa3a65c5236c2df19926ba93d9d43ca07192d989
monacoinproject <monacoinproject@gmail.com>
2017-07-06 02:32:42 +0900

    Monacoin: Bugfix:Allow old private key at import

--- src/base58.h
+++ src/base58.h
@@ -127,9 +127,9 @@ class CBitcoinSecret : public CBase58Data
 public:
     void SetKey(const CKey& vchSecret);
     CKey GetKey();
-    bool IsValid() const;
-    bool SetString(const char* pszSecret);
-    bool SetString(const std::string& strSecret);
+    bool IsValid(bool bOldPrefix = false) const;
+    bool SetString(const char* pszSecret, bool bOldPrefix = false);
+    bool SetString(const std::string& strSecret, bool bOldPrefix = false);
 
     CBitcoinSecret(const CKey& vchSecret) { SetKey(vchSecret); }
     CBitcoinSecret() {}
