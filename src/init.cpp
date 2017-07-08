521afe750f72ed56f1e202038d0190e8d81ba7bd
monacoinproject <monacoinproject@gmail.com>
2017-07-08 12:16:30 +0900

    Monacoin: Update copyright

--- src/init.cpp
+++ src/init.cpp
@@ -515,7 +515,7 @@ std::string LicenseInfo()
     const std::string URL_SOURCE_CODE = "<https://github.com/monacoinproject/monacoin>";
     const std::string URL_WEBSITE = "<https://monacoin.org>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2011, COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2013, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
