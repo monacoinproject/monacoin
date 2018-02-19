f2c03aa85781f3099c638eaf13f9e41b3f0f6485
Adrian Gallagher <thrasher@addictionsoftware.com>
2018-02-18 16:14:53 -0800

    Litecoin: Fix various tests

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -114,7 +114,7 @@ void setupAddressWidget(QValidatedLineEdit *widget, QWidget *parent)
 bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
 {
     // return if URI is not valid or is no bitcoin: URI
-    if(!uri.isValid() || uri.scheme() != QString("bitcoin"))
+    if(!uri.isValid() || uri.scheme() != QString("litecoin"))
         return false;
 
     SendCoinsRecipient rv;
