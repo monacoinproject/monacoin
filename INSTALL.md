f9d0234d687415f9d8165bbf8ee7c905bf4df98f
monacoinproject <monacoinproject@gmail.com>
2018-02-28 14:55:31 +0900

    Monacoin: Minor changes for 0.15

--- INSTALL.md
+++ INSTALL.md
@@ -1,5 +1,5 @@
-Building Bitcoin
+Building Monacoin
 ================
 
 See doc/build-*.md for instructions on building the various
-elements of the Bitcoin Core reference implementation of Bitcoin.
+elements of the Monacoin Core reference implementation of Monacoin.
--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -1,29 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
 SRCDIR=${SRCDIR:-$TOPDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$SRCDIR/monacoind}
-BITCOINCLI=${BITCOINCLI:-$SRCDIR/monacoin-cli}
-BITCOINTX=${BITCOINTX:-$SRCDIR/monacoin-tx}
-BITCOINQT=${BITCOINQT:-$SRCDIR/qt/monacoin-qt}
+MONACOIND=${MONACOIND:-$SRCDIR/monacoind}
+MONACOINCLI=${MONACOINCLI:-$SRCDIR/monacoin-cli}
+MONACOINTX=${MONACOINTX:-$SRCDIR/monacoin-tx}
+MONACOINQT=${MONACOINQT:-$SRCDIR/qt/monacoin-qt}
 
-[ ! -x $BITCOIND ] && echo "$MONACOIND not found or not executable." && exit 1
+[ ! -x $MONACOIND ] && echo "$MONACOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-BTCVER=($($MONACOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+MONAVER=($($MONACOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
 # but has different outcomes for bitcoin-qt and bitcoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$BITCOIND --version | sed -n '1!p' >> footer.h2m
+$MONACOIND --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $BITCOIND $BITCOINCLI $BITCOINTX $BITCOINQT; do
+for cmd in $MONACOIND $MONACOINCLI $MONACOINTX $MONACOINQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${BTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${MONAVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${MONAVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
