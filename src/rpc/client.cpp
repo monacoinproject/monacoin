a7cba6eb77ef08dab84b1be0d55d87dae3443b00
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2019-02-14 17:07:29 +0900

    wallet/rpc: add maxfeerate parameter to testmempoolaccept

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -96,6 +96,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "sendrawtransaction", 1, "allowhighfees" },
     { "testmempoolaccept", 0, "rawtxs" },
     { "testmempoolaccept", 1, "allowhighfees" },
+    { "testmempoolaccept", 1, "maxfeerate" },
     { "combinerawtransaction", 0, "txs" },
     { "fundrawtransaction", 1, "options" },
     { "fundrawtransaction", 2, "iswitness" },
