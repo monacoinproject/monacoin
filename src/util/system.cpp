daddf897ce744e7c86a38477e1762cb1f89a1070
xwatanabek <xwatanabek@gmail.com>
2019-04-24 05:07:28 +0900

    Monacoin: Update copyright

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -1212,8 +1212,28 @@ std::string CopyrightHolders(const std::string& strPrefix)
     // Make sure Bitcoin Core copyright is not removed by accident
     if (copyright_devs.find("Bitcoin Core") == std::string::npos) {
         std::string strYear = strPrefix;
-        strYear.replace(strYear.find("2011"), sizeof("2011")-1, "2009");
+        strYear.replace(strYear.find("2013"), sizeof("2013")-1, "2009");
         strCopyrightHolders += "\n" + strYear + "The Bitcoin Core developers";
+
+        strYear = strPrefix;
+        strYear.replace(strYear.find("2013"), sizeof("2013")-1, "2011");
+        strCopyrightHolders += "\n" + strYear + "The Litecoin Core Developers";
+
+        strYear = strPrefix;
+        strYear.replace(strYear.find("2013"), sizeof("2013")-1, "2011");
+        strCopyrightHolders += "\n" + strYear + "Dr. Kimoto Chan";
+
+        strYear = strPrefix;
+        strYear.replace(strYear.find("2013"), sizeof("2013")-1, "2011");
+        strCopyrightHolders += "\n" + strYear + "The DigiByte developers";
+
+        strYear = strPrefix;
+        strYear.replace(strYear.find("2013"), sizeof("2013")-1, "2014");
+        strCopyrightHolders += "\n" + strYear + "The Dash developers";
+
+        strYear = strPrefix;
+        strYear.replace(strYear.find("2013"), sizeof("2013")-1, "2014");
+        strCopyrightHolders += "\n" + strYear + "The Vertcoin Developers";
     }
     return strCopyrightHolders;
 }
