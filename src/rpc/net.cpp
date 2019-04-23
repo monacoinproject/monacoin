bccf7aa9cebc050f3d2630ae04eba2ee22748ecc
xwatanabek <xwatanabek@gmail.com>
2019-04-24 04:57:00 +0900

    Monacoin: Adjust port strings

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -247,8 +247,8 @@ static UniValue addnode(const JSONRPCRequest& request)
                 },
                 RPCResult{RPCResult::Type::NONE, "", ""},
                 RPCExamples{
-                    HelpExampleCli("addnode", "\"192.168.0.6:9333\" \"onetry\"")
-            + HelpExampleRpc("addnode", "\"192.168.0.6:9333\", \"onetry\"")
+                    HelpExampleCli("addnode", "\"192.168.0.6:9401\" \"onetry\"")
+            + HelpExampleRpc("addnode", "\"192.168.0.6:9401\", \"onetry\"")
                 },
             }.ToString());
 
@@ -290,9 +290,9 @@ static UniValue disconnectnode(const JSONRPCRequest& request)
                 },
                 RPCResult{RPCResult::Type::NONE, "", ""},
                 RPCExamples{
-                    HelpExampleCli("disconnectnode", "\"192.168.0.6:8333\"")
+                    HelpExampleCli("disconnectnode", "\"192.168.0.6:9401\"")
             + HelpExampleCli("disconnectnode", "\"\" 1")
-            + HelpExampleRpc("disconnectnode", "\"192.168.0.6:8333\"")
+            + HelpExampleRpc("disconnectnode", "\"192.168.0.6:9401\"")
             + HelpExampleRpc("disconnectnode", "\"\", 1")
                 },
             }.Check(request);
