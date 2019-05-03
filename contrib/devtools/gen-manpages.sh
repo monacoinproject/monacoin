2d9bd9aa4f94401a9f71a0cf980269a269e8a0d2
Adrian Gallagher <thrasher@addictionsoftware.com>
2019-05-03 07:10:12 -0700

    Litecoin: Update man pages

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -22,7 +22,7 @@ BTCVER=($($BITCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 # This gets autodetected fine for bitcoind if --version-string is not set,
 # but has different outcomes for bitcoin-qt and bitcoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$LITECOIND --version | sed -n '1!p' >> footer.h2m
+$BITCOIND --version | sed -n '1!p' >> footer.h2m
 
 for cmd in $BITCOIND $BITCOINCLI $BITCOINTX $WALLET_TOOL $BITCOINQT; do
   cmdname="${cmd##*/}"
