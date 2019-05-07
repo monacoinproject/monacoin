250da47dc0ec9eefd71048b3174c61e40c9f64a1
Adrian Gallagher <thrasher@addictionsoftware.com>
2019-05-06 23:56:55 -0700

    Litecoin: Add scrypt support to Travis

--- .travis/test_04_install.sh
+++ .travis/test_04_install.sh
@@ -36,3 +36,7 @@ fi
 travis_retry DOCKER_EXEC apt-get update
 travis_retry DOCKER_EXEC apt-get install --no-install-recommends --no-upgrade -qq $PACKAGES $DOCKER_PACKAGES
 
+if [ "$LITECOIN_SCRYPT" = 1 ]; then 
+  travis_retry DOCKER_EXEC pip3 install litecoin_scrypt
+fi
+
