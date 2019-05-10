dbd2631aa684745bf012b91283ba8b3a972ee4fd
Adrian Gallagher <thrasher@addictionsoftware.com>
2019-05-09 19:22:32 -0700

    Litecoin: Multisig code testing

--- contrib/testgen/gen_key_io_test_vectors.py
+++ contrib/testgen/gen_key_io_test_vectors.py
@@ -19,13 +19,15 @@ from binascii import b2a_hex
 from segwit_addr import bech32_encode, decode, convertbits, CHARSET
 
 # key types
-PUBKEY_ADDRESS = 0
+PUBKEY_ADDRESS = 48
 SCRIPT_ADDRESS = 5
+SCRIPT_ADDRESS2 = 50
 PUBKEY_ADDRESS_TEST = 111
 SCRIPT_ADDRESS_TEST = 196
+SCRIPT_ADDRESS_TEST2 = 58
 PUBKEY_ADDRESS_REGTEST = 111
 SCRIPT_ADDRESS_REGTEST = 196
-PRIVKEY = 128
+PRIVKEY = 176
 PRIVKEY_TEST = 239
 PRIVKEY_REGTEST = 239
 
@@ -53,8 +55,10 @@ templates = [
   #                                  None = N/A
   ((PUBKEY_ADDRESS,),         20, (),   (False, 'main',    None,  None), pubkey_prefix, pubkey_suffix),
   ((SCRIPT_ADDRESS,),         20, (),   (False, 'main',    None,  None), script_prefix, script_suffix),
+  ((SCRIPT_ADDRESS2,),        20, (),   (False, 'main',    None,  None), script_prefix, script_suffix),
   ((PUBKEY_ADDRESS_TEST,),    20, (),   (False, 'test',    None,  None), pubkey_prefix, pubkey_suffix),
   ((SCRIPT_ADDRESS_TEST,),    20, (),   (False, 'test',    None,  None), script_prefix, script_suffix),
+  ((SCRIPT_ADDRESS_TEST2,),   20, (),   (False, 'test',    None,  None), script_prefix, script_suffix),
   ((PUBKEY_ADDRESS_REGTEST,), 20, (),   (False, 'regtest', None,  None), pubkey_prefix, pubkey_suffix),
   ((SCRIPT_ADDRESS_REGTEST,), 20, (),   (False, 'regtest', None,  None), script_prefix, script_suffix),
   ((PRIVKEY,),                32, (),   (True,  'main',    False, None), (),            ()),
@@ -67,28 +71,28 @@ templates = [
 # templates for valid bech32 sequences
 bech32_templates = [
   # hrp, version, witprog_size, metadata, output_prefix
-  ('bc',    0, 20, (False, 'main',    None, True), p2wpkh_prefix),
-  ('bc',    0, 32, (False, 'main',    None, True), p2wsh_prefix),
-  ('bc',    1,  2, (False, 'main',    None, True), (OP_1, 2)),
-  ('tb',    0, 20, (False, 'test',    None, True), p2wpkh_prefix),
-  ('tb',    0, 32, (False, 'test',    None, True), p2wsh_prefix),
-  ('tb',    2, 16, (False, 'test',    None, True), (OP_2, 16)),
-  ('bcrt',  0, 20, (False, 'regtest', None, True), p2wpkh_prefix),
-  ('bcrt',  0, 32, (False, 'regtest', None, True), p2wsh_prefix),
-  ('bcrt', 16, 40, (False, 'regtest', None, True), (OP_16, 40))
+  ('ltc',   0, 20, (False, 'main',    None, True), p2wpkh_prefix),
+  ('ltc',   0, 32, (False, 'main',    None, True), p2wsh_prefix),
+  ('ltc',   1,  2, (False, 'main',    None, True), (OP_1, 2)),
+  ('tltc',  0, 20, (False, 'test',    None, True), p2wpkh_prefix),
+  ('tltc',  0, 32, (False, 'test',    None, True), p2wsh_prefix),
+  ('tltc',  2, 16, (False, 'test',    None, True), (OP_2, 16)),
+  ('rltc',  0, 20, (False, 'regtest', None, True), p2wpkh_prefix),
+  ('rltc',  0, 32, (False, 'regtest', None, True), p2wsh_prefix),
+  ('rltc', 16, 40, (False, 'regtest', None, True), (OP_16, 40))
 ]
 # templates for invalid bech32 sequences
 bech32_ng_templates = [
   # hrp, version, witprog_size, invalid_bech32, invalid_checksum, invalid_char
   ('tc',    0, 20, False, False, False),
-  ('tb',   17, 32, False, False, False),
-  ('bcrt',  3,  1, False, False, False),
-  ('bc',   15, 41, False, False, False),
-  ('tb',    0, 16, False, False, False),
-  ('bcrt',  0, 32, True,  False, False),
-  ('bc',    0, 16, True,  False, False),
-  ('tb',    0, 32, False, True,  False),
-  ('bcrt',  0, 20, False, False, True)
+  ('tltc',  17, 32, False, False, False),
+  ('rltc',  3,  1, False, False, False),
+  ('ltc',   15, 41, False, False, False),
+  ('tltc',  0, 16, False, False, False),
+  ('rltc',  0, 32, True,  False, False),
+  ('ltc',   0, 16, True,  False, False),
+  ('tltc',  0, 32, False, True,  False),
+  ('rltc',  0, 20, False, False, True)
 ]
 
 def is_valid(v):
@@ -108,7 +112,7 @@ def is_valid(v):
 
 def is_valid_bech32(v):
     '''Check vector v for bech32 validity'''
-    for hrp in ['bc', 'tb', 'bcrt']:
+    for hrp in ['ltc', 'tltc', 'rltc']:
         if decode(hrp, v) != (None, None):
             return True
     return False
