5ccccd428c2f6c760c41a4b65633d2e555bb4ae2
Mark Karpeles <magicaltux@gmail.com>
2024-11-26 13:20:07 +0900

    fix issues https://github.com/monacoinproject/monacoin/issues/119 and https://github.com/monacoinproject/monacoin/issues/120 by making tests compile

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -386,8 +386,8 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     }
     BOOST_CHECK(pblocktemplate = AssemblerForTest(chainparams).CreateNewBlock(scriptPubKey));
     // Extend to a 210000-long block chain.
-    while (chainActive.Tip()->nHeight < 105120000) {
-        CBlockIndex* prev = chainActive.Tip();
+    while (::ChainActive().Tip()->nHeight < 105120000) {
+        CBlockIndex* prev = ::ChainActive().Tip();
         CBlockIndex* next = new CBlockIndex();
         next->phashBlock = new uint256(InsecureRand256());
         ::ChainstateActive().CoinsTip().SetBestBlock(next->GetBlockHash());
